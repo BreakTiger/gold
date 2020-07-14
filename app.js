411c9d9898912f21bb87f9567e355f2434d0a036
BreakTiger <46531043+BreakTiger@users.noreply.github.com>
2020-07-14 15:41:52 +0800

    tiger 7-14

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ App({
         // console.log(res)
         wx.setStorageSync('code', res.code)
         wx.setStorageSync('openid', 'sns_wa_o2bFa5P3qK1uFbXQOOPCyrlRg3eg')
+
+        wx.setStorageSync('lat', '23.07466308995538')
+        wx.setStorageSync('lng', '113.32205928152793')
       }
     })
     // 获取用户信息
--- pages_one/shop_order/shop_order.js
+++ pages_one/shop_order/shop_order.js
@@ -1,66 +1,88 @@
-// pages/shop_order/shop_order.js
+const app = getApp()
+const http = require('../../request.js')
+import modal from '../../modals.js'
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
+
   data: {
-    time: '预约回收时间'
+    shop_list: [], //预约门店
+
+    time_one: '预约回收日期',
+
+    time_two: '预约回收时间',
+
+    business_list: [],//预约业务
+
+
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+    this.getShop()
+  },
+
+  // 预约门店
+  getShop: function () {
+    let that = this
+    let data = {
+      page: 1,
+      pagesize: 50,
+      lat: wx.getStorageSync('lat'),
+      lng: wx.getStorageSync('lng')
+    }
+    console.log('参数：', data)
+    http.sendRequest('huishou.shoplist', 'post', data).then(function (res) {
+      if (res.error == 0) {
+        that.setData({
+          shop_list: res.list
+        })
+        that.getBusiness()
+      } else {
+        modal.showToast(res.message, 'none')
+      }
+    })
+  },
+
+  //预约业务
+  getBusiness: function () {
+    let that = this
+    let data = {
+      page: 1,
+      pagesize: 50,
+      type: 2
+    }
+    http.sendRequest('huishou.jingying', 'post', data).then(function (res) {
+      console.log(res.list)
+      if (res.error == 0) {
+        that.setData({
+          business_list: res.list
+        })
+      } else {
+        modal.showToast(res.message, 'none')
+      }
+    })
+  },
+
+
+
+
+  //姓名
+  getName: function (e) {
+    console.log(e.detail.value)
+  },
+
+  //电话
+  getPhone: function (e) {
+    console.log(e.detail.value)
+  },
+
+
+  // 提交
+  toSend: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
-  },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
