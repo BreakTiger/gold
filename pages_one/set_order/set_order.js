c85bac809a1add94bb68021e754b1dbe983e3d09
BreakTiger <46531043+BreakTiger@users.noreply.github.com>
2020-07-15 14:47:37 +0800

    tiger 7-15

--- pages_one/set_order/set_order.js
+++ pages_one/set_order/set_order.js
@@ -6,19 +6,56 @@ Page({
 
 
   data: {
-    price: '',
-    gram:'',
-    count_price: ''
+    price: '', //金价
+    count_price: '', //金额
+    gram: '', //克重
+    picture_list: [] //图片
+
   },
 
   onLoad: function (options) {
     let datas = JSON.parse(options.data)
     this.setData({
       price: datas.price,
-      gram:datas.gram,
+      gram: datas.gram,
       count_price: datas.count_price
     })
   },
+
+  //上传图片
+  addPicture: function () {
+    let that = this
+    wx.chooseImage({
+      count: 5,
+      success: function (res) {
+        console.log(res.tempFilePaths)
+        let path = res.tempFilePaths
+        that.setData({
+          picture_list: path
+        })
+      }
+    })
+  },
+
+  //姓名
+  getName: function (e) {
+
+  },
+
+  //回收地址
+  getCity: function (e) {
+
+  },
+
+  //详细地址
+  getAddress: function (e) {
+
+  },
+
+  // 提交
+  send: function () {
+
+  }
 
 
 
