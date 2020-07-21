5b7c53014b3b1e60674641ecf851ac5bd3c36d18
BreakTiger <46531043+BreakTiger@users.noreply.github.com>
2020-07-21 18:35:09 +0800

    tiger 7-21

--- components/once.js
+++ components/once.js
@@ -14,19 +14,32 @@ Component({
 
   data: {
     shop_name: '',
+
     license: '',
+    up_license: '',
+
     picture: [],
     p_list: [],
+
     city_in: '请选择店铺所在的城市',
+
     address: '',
+
     phone: '',
+
     jing: '',
+
     city_rec: '请选择全市上门回收',
+
     area_rec: '',
 
-    king_list: [],
+    kind_list: [], //经营类目
 
-    area_list: []
+    kind_id: '',
+
+    area_list: [],
+
+    area_id: []
   },
 
   created() {
@@ -36,6 +49,7 @@ Component({
 
   methods: {
 
+    //经营类目列表
     getKind: function () {
       let that = this
       let data = {
@@ -52,7 +66,7 @@ Component({
             item.choice = 0
           })
           that.setData({
-            king_list: list
+            kind_list: list
           })
         } else {
           modal.showToast(res.message, 'none')
@@ -66,6 +80,7 @@ Component({
         shop_name: e.detail.value
       })
     },
+
     // 执照
     getlicense: function () {
       let that = this
@@ -162,7 +177,7 @@ Component({
         city: city_rec
       }
       http.sendRequest('huishou.geuarea', 'post', data).then(function (res) {
-        console.log(res.list)
+        // console.log(res.list)
         if (res.error == 0) {
           let area = res.list
           area.forEach(function (item) {
@@ -174,6 +189,57 @@ Component({
         } else {
           modal.showToast(res.message, 'none')
         }
+      })
+    },
+
+    //选择经营类目
+    choice_kind: function (e) {
+      let that = this
+      let list = that.data.kind_list
+      let index = e.currentTarget.dataset.index
+      list.forEach(function (item, indexs) {
+        if (indexs == index) {
+          if (item.is_true == 0) {
+            item.choice = 1
+            console.log(item.id)
+            that.setData({
+              kind_id: item.id
+            })
+          }
+        } else {
+          if (item.is_true == 0) {
+            item.choice = 0
+          }
+        }
+      })
+      that.setData({
+        kind_list: list
+      })
+    },
+
+    //选择回收区域
+    choice_area: function (e) {
+      let that = this
+      let list = that.data.area_list
+      let index = e.currentTarget.dataset.index
+      let ids = []
+      list.forEach(function (item, indexs) {
+        if (indexs == index) {
+          if (item.choice == 0) {
+            item.choice = 1
+          } else {
+            item.choice = 0
+          }
+        }
+        if (item.choice == 1) {
+          let id = item.id
+          ids.push(id)
+        }
+      })
+      // console.log(ids)
+      that.setData({
+        area_list: list,
+        area_id: ids
       })
     },
 
@@ -197,7 +263,63 @@ Component({
         modal.showToast('请输入店铺的详细地址', 'none')
       } else if (!that.data.phone) {
         modal.showToast('请输入手机号码', 'none')
+      } else if (!that.data.kind_id) {
+        console.log(that.data.kind_id)
+        modal.showToast('请选择经营类目', 'none')
+      } else if (that.data.area_id.length == 0) {
+        modal.showToast('请选择回收的城市，及区域', 'none')
+      } else {
+        that.upLicense()
       }
+
+    },
+
+    //上传 - 营业执照
+    upLicense: async function () {
+      let that = this
+      let path = that.data.license
+      await http.upLoading(path, { type: 1 }).then(function (res) {
+        let result = JSON.parse(res)
+        console.log(result)
+        if (result.error == 0) {
+          that.setData({
+            up_license: result.list.url
+          })
+        } else {
+          modal.showToast(result.message, 'none')
+        }
+      })
+
+      let list = that.data.picture
+      if (list.length != 0) {
+        that.upList(list)
+      } else {
+        that.toSent()
+      }
+    },
+
+    //上传店铺图片
+    upList: async function (list) {
+      let that = this
+      let a = []
+      for (let i = 0; i < list.length; i++) {
+        await http.upLoading(list[i], { type: 1 }).then(function (res) {
+          console.log(res)
+          if (result.error == 0) {
+            a.push(result.list.url)
+          } else {
+            modal.showToast(result.message, 'none')
+          }
+        })
+      }
+      that.setData({
+        p_list: a
+      })
+      that.sent()
+    },
+
+    //提交
+    sent: function () {
 
     }
 
